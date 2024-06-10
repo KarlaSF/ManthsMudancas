76096938ec61fae39dbe13c7ac381972ca34a527
KarlaSF <karlaeduarda.faustino@gmail.com>
2024-06-09 22:45:33 -0300

    first commit

---
+++ Manthano/src/app/paginas/pprincipal/pprincipal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pprincipal',
+  standalone: true,
+  imports: [],
+  templateUrl: './pprincipal.component.html',
+  styleUrl: './pprincipal.component.css'
+})
+export class PprincipalComponent {
+
+}
